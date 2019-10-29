a7e7959a33153551339e9bda3f4f75b6ff58475e
Samuel Riegler <riegl2s@cmich.edu>
2019-10-29 14:28:02 -0400

    the comments are done for all code. The report still requres finishing

--- 480hw4group/src/Main.java
+++ 480hw4group/src/Main.java
@@ -32,7 +32,6 @@ public class Main {
     private final static String WriteFile = "Report.txt";
     
     private static Random rnd = new Random(RANDSEED);
-
     
     private static File file = new File(f1);
     
@@ -53,33 +52,40 @@ public class Main {
 	}
 	
 	/**
-	 * 
+	 * This method is responsible for going through the correct number of generations
+	 * and then calling the helping methods at the correct time
 	 * @throws IOException
 	 */
 	private static void run() throws IOException {
 		start = System.currentTimeMillis();
 		double last = 0;
-		int counter = 0;;
+		int counter = 0;
+		// go through the correct number of generations
 		for(int i = 0; i < MAXGEN; i++) {
 			sortLowest();
 			
 			generationNumber = i + 1;
 			
+			// if the fitness has not gotten better add to the counter
 			if(last == population[0].fitness) {
 				counter++;
 			}else {
+				// if the fitness has changed reset the counter
 				counter = 0;
 				last = population[0].fitness;
 				System.out.println("Fitness: " + population[0].fitness + "\t\tGeneration: " + i);
 			}
 			
+			// if the counter has reached REPETE_TO_END then the solution is not getting any better
 			if(counter == REPETE_TO_END) {
 				System.out.println("ended with best solution");
+				// break out of the loop
 				break;
 			}
 			
 			// generate children
 			genChildren();
+			// mutate the children
 			mutateChildren();
 		}
 		System.out.println("Reached maximum number of generations");
@@ -88,10 +94,11 @@ public class Main {
 	}
 	
 	/**
-	 * 
+	 * This method mutates the children that have been created on the current generation
 	 */
 	private static void mutateChildren() {
 		int posToStart = (int) (POPSIZE * BESTRATE) + 1;
+		// start where the parents end and go through all the children
 		while(posToStart < population.length) {
 			population[posToStart].mutate();
 			posToStart++;
@@ -100,7 +107,9 @@ public class Main {
 	
 	
 	/**
-	 * 
+	 * Generates all the children from the parents.
+	 * this is done by figuring out how many children for each parent 
+	 * there needs to be and then coping each parent that number of times.
 	 */
 	private static void genChildren() {
 		int counter = 0;
@@ -108,7 +117,9 @@ public class Main {
 		int startIndx = numToKeep + 1;
 		int numOfChild = Math.floorDiv(population.length - startIndx, startIndx);
 		
+		// go through all the parents
 		for(int i = 0; i < numToKeep; i++) {
+			// copy the selected parent the correct number of times
 			for(int j = 0; j < numOfChild; j++) {
 				population[startIndx] = new Node(population[0].path.length);
 				population[startIndx].copy(population[i].path);
@@ -126,12 +137,13 @@ public class Main {
 	
 
 	/**
-	 * 
+	 * This sorts the population based on fitness.
 	 */
 	private static void sortLowest() {
 		double lowest;
 		int lowestIndex = 0;
 		
+		// only find the top x percent of the population
 		for(int i = 0; i < (int) (POPSIZE * BESTRATE); i++) {
 			lowest = Integer.MAX_VALUE;
 			for(int j = i; j < population.length; j++) {
@@ -148,9 +160,9 @@ public class Main {
 	
 
 	/**
-	 * 
-	 * @param x
-	 * @param y
+	 * Swaps nodes at position x and y
+	 * @param x - first position
+	 * @param y - second position
 	 */
 	private static void swap(int x, int y) {
 		Node temp = population[x];
@@ -160,41 +172,51 @@ public class Main {
 	
 
 	/**
-	 * 
+	 * This initializes the starting population randomly
 	 */
 	private static void genPopulation() {
+		// create a node to copy
 		Node master = new Node(city.size());
+		// set a path
 		for(int i = 0; i < city.size(); i++) {
 			master.setPathPos(i, i);
 		}
+		// Shuffle the path
 		master.shuffel();
 	
+		// Initialize the population
 		population = new Node[POPSIZE];
 		
+		// copy the master node the correct number of times
 		for(int i = 0; i < POPSIZE; i++) {
 			Node node = new Node(city.size());
 			node.copy(master.path);
+			// Shuffle every node
 			node.shuffel();
+			// add the node to the population
 			population[i] = node;
 		}
 	}
 	
 
 	/**
-	 * 
+	 * Initialize the data array with the data from the file. 
+	 * then call genPopulation()
 	 */
 	private static void init() {
 		String s;
-		Scanner sc;
+		Scanner sc = null;
 		String[] ci;
 		int counter = 0;
 		
 		try {
+			// create a new scanner for the file
 			sc = new Scanner(file);
 			
 		}catch(Exception e) {
+			// if we cant open it print we cant open the file and exit
 			System.out.println("cant open / read file");
-			return;
+			System.exit(0);
 		}
 		
 		// get the number of cities and set a number the the city
@@ -204,7 +226,6 @@ public class Main {
 			
 			for(int i = 1; i < ci.length; i++) {
 				city.add(ci[i]);
-//				System.out.print(ci[i] + "\t\t");
 			}
 			
 		}
@@ -213,51 +234,57 @@ public class Main {
 		// fill the data
 		data = new double[city.size()][city.size()];
 		
+		// while there is data left to read in the file
 		while(sc.hasNext()) {
+			// get the line
 			s = sc.nextLine();
+			// split the line by commas
 			ci = s.split(",");
 			
+			// go through the split line and add the data
 			for(int i = 1; i < ci.length; i++) {
 				if(ci[i].isEmpty()) {ci[i] = "0.0";}
 				data[i - 1][counter] = Double.parseDouble(ci[i]);
-//				System.out.print(ci[i] + "\t\t");
 			}
 			
 			System.out.println("");
 			counter++;
 		}
+		// close the scanner
 		sc.close();
 		genPopulation();
-		
 	}
 	
 	/**
-	 * 
-	 * @param x
-	 * @param y
-	 * @return
+	 * Gets the data form the 2d array at the correct position
+	 * @param x - starting city
+	 * @param y - ending city
+	 * @return double that is the distance from x to y
 	 */
 	public static double getData(int x, int y) {
-		//System.out.println("data: " + data[x][y] + "\tx: " + x + "\ty: " + y);
 		return data[x][y];
 	}
 	
 
 	/**
-	 * 
-	 * @return
+	 * gets MUTRATE
+	 * @return the mutation rate as a double
 	 */
 	public static double getMutationRate() {
 		return MUTRATE;
 	}
 
+	/**
+	 * returns a random number
+	 * @return double that is the next random number
+	 */
 	public static double getRandom() {
 		return rnd.nextDouble();
 	}
 
 
 	/**
-	 * 
+	 * prints the data to the data file.
 	 * @throws IOException
 	 */
 	private static void printToFile() throws IOException {
@@ -273,8 +300,8 @@ public class Main {
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * formats the time into a nice looking format
+	 * @return a string that says the time
 	 */
 	private static String findTime() {
 		timeMS = end - start;
@@ -286,12 +313,4 @@ public class Main {
 			return (timeS + " seconds");
 		}
 	}
-
-
-
-
-
-
-
-
 }
--- 480hw4group/src/Node.java
+++ 480hw4group/src/Node.java
@@ -1,15 +1,27 @@
+/**
+ * This class represents the individual in the population. It has the ability to 
+ * mutate, shuffle its self, and calculate its fitness.
+ * 
+ * @author Samuel Riegler
+ *
+ */
+
 public class Node {
 	
 	double fitness;
 	int[] path;
 	
+	/**
+	 * Constructor for node
+	 * @param length - length of the path
+	 */
 	public Node(int length) {
 		path = new int[length];
 	}
 	
 	/**
-	 * 
-	 * @param toCopy
+	 * Copy's an a passed path to this path
+	 * @param toCopy - a path to copy
 	 */
 	public void copy(int [] toCopy) {
 		for(int i = 0; i < toCopy.length; i++) {
@@ -18,27 +30,32 @@ public class Node {
 	}
 	
 	/**
-	 * 
+	 * mutates this.path by swapping two points. 
 	 */
 	public void mutate() {
 		double mutRate = Main.getMutationRate();
+		// decide if we mutate
 		if(Main.getRandom() <= mutRate) {
+			// if we mutate get two random positions
 			int pos1 = (int) (Main.getRandom() * path.length);
 			int pos2 = (int) (Main.getRandom() * path.length);
 			
+			// make sure the positions are not the same
 			while (pos1 == pos2) {
 				pos2 = (int) (Main.getRandom() * path.length);
 			}
 			
+			// swap the positions
 			int temp = path[pos1];
 			path[pos1] = path[pos2];
 			path[pos2] = temp;
 		}
+		// calculate the new fitness
 		calcFitness();
 	}
 	
 	/**
-	 * 
+	 * calculates the fitness of the path
 	 */
 	public void calcFitness() {
 		fitness = 0;
@@ -48,27 +65,31 @@ public class Node {
 	}
 	
 	/**
-	 * 
-	 * @param pos
-	 * @param value
+	 * inserts a position, value pare into the path
+	 * @param pos - the position for it to be inserted
+	 * @param value - the value to be inserted
 	 */
 	public void setPathPos(int pos, int value) {
 		path[pos] = value;
 	}
 	
 	/**
-	 * 
+	 * Shuffles the path to generate a random path
 	 */
 	public void shuffel() {
 		int pos1, pos2, temp;
 		
+		// swap positions the number of times equal to the path length
 		for(int i = 0; i < path.length; i++) {
+			// choose two positions
 			pos1 = (int) (Main.getRandom() * path.length);
 			pos2 = (int) (Main.getRandom() * path.length);
+			// swap them
 			temp = path[pos1];
 			path[pos1] = path[pos2];
 			path[pos2] = temp;
 		}
+		// calculate the new fitness
 		calcFitness();
 	}
 }
